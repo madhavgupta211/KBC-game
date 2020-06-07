1ec75049c7fa5e82f8ca1d51a158da14752c67bf
madhavgupta211 <f20190063@pilani.bits-pilani.ac.in>
2020-06-07 18:52:31 +0530

    admin page login

--- fundamentals.h
+++ fundamentals.h
@@ -5,12 +5,15 @@
 #include <Windows.h> //for getting the rows and columns of terminal window
 #include <string>
 #include <iomanip>
+#include <stdlib.h>
+#include <conio.h>
 
 // display_intro asks from the user whether he wants to access
 // developer page or just wants to play the game
 // returns 1 if admin page is chosen, 2 otherwise  
 int display_intro()
 {
+    system("cls");
     CONSOLE_SCREEN_BUFFER_INFO csbi;   //windows.h object
     int rows,columns,access_value;                  
     GetConsoleScreenBufferInfo( GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
@@ -38,4 +41,40 @@ int display_intro()
     return access_value;
 }
 
+// admin_login displays the login screen for admin framework and asks for password
+//return true if login successful, otherwise false
+bool admin_login(std::string password, int no_of_attempts)
+{
+    system("cls");
+    CONSOLE_SCREEN_BUFFER_INFO csbi;   //windows.h object
+    int rows,columns,compare_result;
+    std::string input_text;                                              //password entered by user to be checked                  
+    GetConsoleScreenBufferInfo( GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;              //calculated the no. of rows and columns of the terminal window
+    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;                 //by using windows.h header file
+    std::string title = "KBC: The Game";
+    std::cout<<std::setw((columns/2)+title.length()/2)<<std::right<<std::setfill('*');  //center aligned the title line
+    std::cout<< title;
+    for(int i=0;i<((columns/2)-title.length()/2);i++)
+    {
+        std::cout<<"*";
+    }
+    std::cout<<"\n\n\n";
+    std::string number = "No. of attempts remaining :";
+    std::cout<<std::setw((columns/2)+number.length()/2)<<std::right<<std::setfill(' ');
+    std::cout<<number<<" "<<no_of_attempts;
+    std::cout<<"\n";
+    std::string enter = "Enter Password:";
+    std::cout<<std::setw((columns/2)+enter.length()/2)<<std::right<<std::setfill(' ');
+    std::cout<<enter;
+    std::cin>>input_text;                                               // takes input the password from user
+    compare_result = input_text.compare(password);
+    if(compare_result==0)
+    {
+        return true;
+    }
+    std::cout<<"Access Denied! Try again";
+    getch();
+    return false;
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream> 
 #include "fundamentals.h"
+#define NO_OF_ATTEMPTS 5      //no. of attempts to login
+#define PASSWORD "cholosolo"  //change admin's password from here
 
-using namespace std;
+using namespace std; 
 
 int main()
 {
     int access_type;
     access_type = display_intro();
-    cout<<access_type;
+    if(access_type==1)
+    {
+        bool admin_access_status = false;
+        for(int i=NO_OF_ATTEMPTS;i>0;i--)
+        {
+            admin_access_status = admin_login(PASSWORD,i);
+            if(admin_access_status)
+            {
+                break;
+            }
+        }
+        if(admin_access_status == false)
+        {
+            cout<<"\nAccess Denied!! No attempts left!! Exiting...";
+            exit(1);
+        }
+        cout<<"\nAccess granted.";
+    }
     return 0;
 }
